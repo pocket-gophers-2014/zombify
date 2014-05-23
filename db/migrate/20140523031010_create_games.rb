3befcecaa9546e2652067c05fd919c56fc9ec78c
Maggy Feldman <Mahkt6@gmail.com>
2014-05-23 14:34:23 -0700

    adds DB defaults

--- db/migrate/20140523031010_create_games.rb
+++ db/migrate/20140523031010_create_games.rb
@@ -1,10 +1,13 @@
 class CreateGames < ActiveRecord::Migration
   def change
   	create_table :games do |t|
-  		t.string :city
-  		t.string :state
-  		t.string :title
+  		t.string :city, :default => 'San Francisco'
+      t.string :game_code
+  		t.string :state, :default => 'California'
+  		t.string :title, :default => 'Zombie Apocalypse'
+      t.boolean :game_active, :default => false
   		t.datetime :start_time
+      t.datetime :end_time
   		t.datetime :end_time
   		t.timestamps
   	end
--- db/migrate/20140523031106_add_messages.rb
+++ db/migrate/20140523031106_add_messages.rb
@@ -4,6 +4,7 @@ class AddMessages < ActiveRecord::Migration
       t.string :title
       t.string :description
       t.string :audience
+      t.boolean :has_been_called, :default => false
       t.belongs_to :event
     end
   end
