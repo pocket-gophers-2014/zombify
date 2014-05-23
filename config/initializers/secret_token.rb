2227172b3551fc03f1a8acf5ced6b363e44c3dd2
& <pair+cyancey.nickmeehan@devbootcamp.com>
2014-05-22 18:19:04 -0700

    Adds rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zombify::Application.config.secret_token = 'e18ad2c16fbe51c80aeaea6dedf7b13434672d071a951dc53c704495e66a99a34553b6f09ec9fe872bff4da3c58ee5d90e821901e97ebca7a5b04702d80b8a21'
