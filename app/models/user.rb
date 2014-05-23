0d3ad9f566e1c215aa4099a5febd92e4ccfd93a7
Carolyn Atterbury <carolynatterbury@Carolyns-MacBook-Air.local>
2014-05-23 11:04:04 -0700

    adds factories file to create users, creates passing tests for the user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	attr_accessible :email, :phone_number, :password
 	belongs_to :game
+  validates :email, :presence => true
   has_secure_password
 
   def password
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,30 @@ require 'spec_helper'
 
 describe UsersController do
 
+  context "show" do
+    let(:user) { FactoryGirl.create :user }
+    it "finds a specific user" do
+      get :show, :id => user.id
+      expect(assigns(:user)).to eq(user)
+    end
+  end
+
+  context "create" do
+    it "enters a valid user into the database" do
+      User.destroy_all
+      params = { user: { email: "awesome" , password: "awesome" } }
+      get :create, params
+      expect(User.all.count).to eq(1)
+    end
+
+    it "does not enter a user with invalid username into the database" do
+      User.destroy_all
+      params = { user: { email: '', password: "awesome" } }
+      get :create, params
+      expect(User.all.count).to eq(0)
+    end
+  end
 end
+
+
+
