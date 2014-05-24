8c66661449f4a2fde28ee52bab72e654dec1741c
Steven G. Harms <steven@devbootcamp.com>
2014-05-24 07:06:07 -0700

    Jasmine Jquery: rm unhelpful tests

--- spec/javascripts/users_spec.js
+++ spec/javascripts/users_spec.js
@@ -13,29 +13,4 @@ describe("UserAuthenticationAjax", function () {
       expect(UserAuthenticationAjax.initiateLogIn).toHaveBeenCalled()
     });
   });
-
-
-  // beforeEach( function() {
-  //   var spyEvent = spyOnEvent($('#login'), 'click');
-  // });
-
-  // it("clicks the #login", function() {
-  //   $("#login").click();
-  //   expect('click').toHaveBeenTriggeredOn($('#login'));
-  // });
-
-
-  // describe("initiateLogIn", function() {
-  //     var configuration = { url: "/sessions/new",
-  //                           method: "get"
-  //                         };
-  // });
-
-  // it("should make an Ajax request to the correct URL", function() {
-  //   spyOn($, "ajax");
-  //   sendRequest(undefined, configuration);
-  //   expect($.ajax.mostRecentCall.args[0]["url"]).toEqual(configuration.url);
-  // });
 });
-
-
