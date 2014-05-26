814ee6e0aa90fc0ca766dabc10334a6b5710fa0f
Carolyn Atterbury <carolynatterbury@Carolyns-MacBook-Air.local>
2014-05-25 18:16:44 -0700

    methods to calculate statistics and create posts based on statistics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,8 +57,6 @@ class UsersController < ApplicationController
 
   def verify_results(params)
     @winner = eval_string(params["result"])
-    p @winner
-    p "WINNER WOOOWOWOWOWOWOWOWOWWOWOWOWOW"
     handle = parse_opponent_id(params["opponent"])
     @opponent = User.find_by_handle(handle) || "invalid"
     return @result = "invalid user code" if @opponent == "invalid"
@@ -75,43 +73,40 @@ class UsersController < ApplicationController
     elsif !@user.infected && !@opponent.infected
       return @result = "Why are you wasting precious cures?!"
     elsif @user.infected && @winner ##zombie user bites human
-      Post.new(body:"#{@user.name} has bitten #{@opponent.name}", title:"New Zombie", audience:"both")
+      Post.create(body:"#{@user.name} has bitten #{@opponent.name}", title:"New Zombie", audience:"both")
       @user.update_attributes(points: @user.points += 300)
-      @user.save
       @opponent.update_attributes(infected: true)
-      @opponent.save
+      check_stats
       return @result = "Mmmmmm....Brainsssss.....You have added to the horde."
     elsif @user.infected && !@winner ##zombie user misses human
       p @winner
-      Post.new(body:"#{@opponent.name} has escaped #{@user.name}", title:"Near Miss", audience:"both")
+      Post.create(body:"#{@opponent.name} has escaped #{@user.name}", title:"Near Miss", audience:"both")
       @opponent.update_attributes(points: @opponent.points += 100)
-      @opponent.save
+      check_stats
       return @result = "You are feeling dizzy. The human has escaped. You still crave brains... "
     elsif @user.can_cure && @winner ##human cures zombie
-      Post.new(body:"#{@opponent.name} has been cured by #{@user.name}", title:"Human Reversion", audience:"both")
+      Post.create(body:"#{@opponent.name} has been cured by #{@user.name}", title:"Human Reversion", audience:"both")
       @user.update_attributes(points: @user.points += 500)
-      @user.save
       @opponent.update_attributes(infected: false)
-      @opponent.save
+      check_stats
       return @result = "You have successfully applied the cure!"
     elsif @user.can_cure && !@winner ##human fails zombie cure
-      Post.new(body:"#{@user.name} failed a cure attempt on #{@opponent.name}", title:"Cure Failed", audience:"both")
-      Post.new(body:"#{@opponent.name} has bitten #{@user.name}", title:"New Zombie", audience:"both")
+      Post.create(body:"#{@user.name} failed a cure attempt on #{@opponent.name}", title:"Cure Failed", audience:"both")
+      Post.create(body:"#{@opponent.name} has bitten #{@user.name}", title:"New Zombie", audience:"both")
       @user.update_attributes(infected: true)
-      @user.save
       @opponent.update_attributes(points: @opponent.points += 100)
+      check_stats
       return @result = "Your cure has failed. You feel your blood rising and crave delicious brains..."
     elsif @user.can_cure == false
       @opponent.update_attributes(points: @opponent.points += 100)
-      @user.save
       @opponent.update_attributes(infected: false)
-      @opponent.save
       ##create 2 new posts, one with zombie audience, one with human audience
       ##update both users if they have earned points
       ##update people's secret codes?
       ##update session?
       ##check the conditonals in the rest of the game logic
       ##return text to render to the battle result page
+      check_stats
       return @result = "You do not have the cure! What are you doing!?"
     else
       return @result = "Something has gone wrong."
@@ -127,6 +122,35 @@ class UsersController < ApplicationController
   def eval_string(boolean_string)
     boolean_string == 'true'
   end
-end
+
+  def check_stats
+    if Stats.all_human?
+      @message = Message.human_messages.last
+      Post.create(title: @message[0], body: @message[1], audience: "human")
+    elsif Stats.all_zombie?
+      @message = Message.zombie_messages.last
+      Post.create(title: @message[0], body: @message[1], audience: "zombie")
+    else
+      zombies_percentage = Stats.percent_zombies
+      remaining_stats(zombies_percentage)
+    end
+  end
+
+  def remaining_stats(zombie_percentage)
+    p zombie_percentage
+    p "============================="
+    zombie_messages = Message.zombie_messages
+    human_messages = Message.human_messages
+    case zombie_percentage
+      when 90..93
+        Post.create(title: zombie_messages[7][0], body: zombie_messages[7][1],audience: "zombie")
+        Post.create(title: human_messages[7][0], body: human_messages[7][1],audience: "human")
+      when 50..53
+        Post.create(title: zombie_messages[6][0], body: zombie_messages[6][1],audience: "zombie")
+        Post.create(title: human_messages[6][0], body: human_messages[6][1], audience: "human")
+      end
+    end
+  end
+
 
 
