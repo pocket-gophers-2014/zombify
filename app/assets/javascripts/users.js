f58cf564da69ca00dc7c5223d33983bf90ef270e
Maggy Feldman <Mahkt6@gmail.com>
2014-05-26 01:43:16 -0700

    ready for master merge

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -31,7 +31,7 @@ UserAuthenticationAjax.prototype = {
 
 $( document ).ready(function() {
   view = new View();
-  userAuth = new UserAuthenticationAjax(view)
+  userAuth = new UserAuthenticationAjax(view);
   userAuth.bindEvents();
   var polling = new pollingController(view);
   polling.startPolling();
