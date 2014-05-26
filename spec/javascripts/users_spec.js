df061fcca2681c20f14ac8021acc0b481d5e5164
Carolyn Atterbury <carolynatterbury@Carolyns-MacBook-Air.local>
2014-05-26 14:59:46 -0700

    still having trouble with fixtures, test pending

--- spec/javascripts/users_spec.js
+++ spec/javascripts/users_spec.js
@@ -1,16 +1,14 @@
-// describe("UserAuthenticationAjax", function () {
-//   describe("bindEvents", function() {
-//     it("binds a click event on login", function() {
-//       spyOn(UserAuthenticationAjax, 'initiateLogIn');
-//       loadFixtures('login.html');
-//       // Obviously, adding the click handler in the test is the Wrong Way To Do
-//       // It (tm).  However, it serves to get the test passing and shows where
-//       // you need make a change in your implementation so that this test
-//       // passes.
-//       $('#login').on('click', function() {
-//         UserAuthenticationAjax.initiateLogIn();
-//       }).click();
-//       expect(UserAuthenticationAjax.initiateLogIn).toHaveBeenCalled()
-//     });
-//   });
-// });
+describe("UserAuthenticationAjax", function () {
+  describe("bindEvents", function() {
+    xit("binds a click event on login", function() {
+
+      loadFixtures('login.html');
+      spyOn(UserAuthenticationAjax, 'initiateLogIn');
+
+      $('#login').on('click', function() {
+        UserAuthenticationAjax.initiateLogIn();
+      }).click();
+      expect(UserAuthenticationAjax.initiateLogIn).toHaveBeenCalled()
+    });
+  });
+});
