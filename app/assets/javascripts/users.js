96e83ccc92b2b6b7045fc71d36474dba3ab7e00d
Carolyn Atterbury <carolynatterbury@Carolyns-MacBook-Air.local>
2014-05-26 10:24:21 -0700

    added user code front page

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -37,7 +37,6 @@ $( document ).ready(function() {
   polling.startPolling();
   BattleController.bindEvents(polling);
   UserAuthenticationAjax.bindEvents();
-  BattleController.bindEvents();
   CheckinController.bindEvents();
 });
 
