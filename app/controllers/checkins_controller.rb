efa0c845a917d81e6745ce703b97d5e6b929f150
Andrew Fruth <anfruth@gmail.com>
2014-05-26 17:32:28 -0700

    Still working on refactor of checkins_controller

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,15 +1,16 @@
 class CheckinsController < ApplicationController
   def new
-    CONSTANTS = {meters_within_ingredient => 100, pts_gained_find_ingredient => 200, 
-      num_needed_to_harvest => 10}
+    @constants = {:meters_within_ingredient => 1000, :pts_gained_find_ingredient => 200, 
+      :num_needed_to_harvest => 15}
     @current_ingredient = Ingredient.where(discovered: true, harvested: false).first
     @user = User.find(session[:id])
-
     ingredient_in_range? ? user_success_logic : show_failure_message
 
     render :text => @response 
-
   end
+
+  ## ONLY HELPER METHODS BELOW
+  ## ===================================
 
   def distance(a, b)
     rad_per_deg = Math::PI/180  # PI / 180
@@ -29,23 +30,24 @@ class CheckinsController < ApplicationController
   end
 
   def user_success_logic
+    p "SHIT"
     user_gains_points
     increment_ingredient_counter
     create_message_showing_personal_harvest
-    complete_grou_harvest_if_possible
+    complete_group_harvest_if_possible
   end
 
 
   def ingredient_in_range?
-    distance([@current_ingredient.latitude.to_f, @current_ingredient.longitude.to_f], [params[:userLat].to_f, params[:userLong].to_f]) < CONSTANTS[meters_within_ingredient]
+    distance([@current_ingredient.latitude.to_f, @current_ingredient.longitude.to_f], [params[:userLat].to_f, params[:userLong].to_f]) < @constants[:meters_within_ingredient]
   end
 
   def user_gains_points
-    @user.update_attributes(points: @user.points += CONSTANTS[pts_gained_find_ingredient])
+    @user.update_attributes(points: @user.points += @constants[:pts_gained_find_ingredient])
   end
 
   def increment_ingredient_counter
-    @current_ingredient.update_attributes(counter: @current_ingredient.counter += 1)
+    @current_ingredient.update_attribute(:counter, @current_ingredient.counter += 1)
   end
 
   def create_message_showing_personal_harvest
@@ -54,14 +56,17 @@ class CheckinsController < ApplicationController
   end
 
   def complete_group_harvest_if_possible
-    if @current_ingredient.counter > CONSTANTS[num_needed_to_harvest]
+    if @current_ingredient.counter == @constants[:num_needed_to_harvest]
       @current_ingredient.update_attributes(harvested: true)
 
-      @zombie_message = Message.where(title: "#{@current_ingredient} gathered", audience: "zombie") 
-      @human_message = Message.where(title: "#{@current_ingredient} gathered", audience: "human") 
+      p "#{@current_ingredient} gathered"
+      @zombie_message = Message.where(title: "#{@current_ingredient.name} gathered", audience: "zombie")[0]
+      p 'FUCKERS'
+      p @zombie_message.title
+      @human_message = Message.where(title: "#{@current_ingredient.name} gathered", audience: "human")[0] 
 
-      Post.create(body: @zombie_message.body, title: @zombie_message.title, audience: "zombie")
-      Post.create(body: @human_message.body, title: @human_message.title, audience: "human")
+      Post.create(body: @zombie_message.description, title: @zombie_message.title, audience: "zombie")
+      Post.create(body: @human_message.description, title: @human_message.title, audience: "human")
 
       @zombie_message.update_attributes(has_been_called: "true")
       @human_message.update_attribues(has_been_called: "true")
