ec73c421788ad8b6f2e96b1da923108f0b9f9756
Maggy Feldman <Mahkt6@gmail.com>
2014-05-27 15:20:34 -0700

    fixes humans biting each other and humans biting self

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,10 @@ class Post < ActiveRecord::Base
   attr_accessible :body, :title, :audience
 
   def self.latest_zombie_posts
-    Post.where("audience = ? or audience = ?", "zombie", "both").limit(20).reverse
+    Post.where("audience = ? or audience = ?", "zombie", "both").reverse
   end
 
   def self.latest_human_posts
-    Post.where("audience = ? or audience = ?", "human", "both").limit(20).reverse
+    Post.where("audience = ? or audience = ?", "human", "both").reverse
   end
 end
